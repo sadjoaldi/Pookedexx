35bea29f3539099405cd923fe0379da41fb271e7
sadjoaldi <alsannealdi@hotmail.com>
2025-01-21 15:35:55 +0100

    first component within bulbasur

---
+++ src/components/PokemonCard.tsx
@@ -0,0 +1,15 @@
+const PokemonCard = () => {
+  return (
+    <>
+      <figure>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
+          alt=""
+        />
+        <figcaption>Bulbasaur</figcaption>
+      </figure>
+    </>
+  );
+};
+
+export default PokemonCard;
