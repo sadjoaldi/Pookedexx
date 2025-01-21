bed2b1ea383688390a2dbd709bb78330d6235440
sadjoaldi <alsannealdi@hotmail.com>
2025-01-21 16:27:41 +0100

    pokemon dynamique

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,27 @@
+const pokemonList = [
+  {
+    name: "bulbasaur",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "mew",
+  },
+];
+
 const PokemonCard = () => {
+  const pokemon = pokemonList[1];
+
   return (
     <>
       <figure>
-        <img
-          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png"
-          alt=""
-        />
-        <figcaption>Bulbasaur</figcaption>
+        {pokemon.imgSrc ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name} />
+        ) : (
+          "???"
+        )}
+
+        <figcaption> {pokemon.name} </figcaption>
       </figure>
     </>
   );
