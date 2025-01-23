2d87088c7702bc24049bd8a5d41ef599cfaf98cb
sadjoaldi <alsannealdi@hotmail.com>
2025-01-23 12:09:08 +0100

    utilisation de props

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,24 +1,11 @@
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-const PokemonCard = () => {
-  const pokemon = pokemonList[1];
-
+const PokemonCard = ({ pokemon }) => {
   return (
     <>
       <figure>
         {pokemon.imgSrc ? (
           <img src={pokemon.imgSrc} alt={pokemon.name} />
         ) : (
-          "???"
+          <p>"???"</p>
         )}
 
         <figcaption> {pokemon.name} </figcaption>
