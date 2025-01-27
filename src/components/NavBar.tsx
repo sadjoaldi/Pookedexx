4067f1164db09ccfc1f388aa8995eed05d7b2d8b
sadjoaldi <alsannealdi@hotmail.com>
2025-01-27 01:44:41 +0100

    pika pikachu

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,25 +1,39 @@
+import { useState } from "react";
+
 interface Pokemon {
   name: string;
   imgSrc?: string;
 }
 
 interface NavBarProps {
-  setPokemonIndex: (index: number) => void;
+  setPokemonIndex: (id: number) => void;
   pokemonList: Pokemon[];
 }
 
 export default function NavBar({ pokemonList, setPokemonIndex }: NavBarProps) {
+  const [pikaMes, setpikaMes] = useState("");
+
+  const handleClick = (pokemonName: string, id: number) => {
+    setPokemonIndex(id);
+    if (pokemonName === "pikachu") {
+      setpikaMes("pika pikachu !!!");
+    } else {
+      setpikaMes("");
+    }
+  };
+
   return (
     <div>
       <nav>
         {pokemonList.map((pokemon, key) => {
           return (
-            <button key={key} onClick={() => setPokemonIndex(key)}>
+            <button key={key} onClick={() => handleClick(pokemon.name, key)}>
               {pokemon.name}
             </button>
           );
         })}
       </nav>
+      {pikaMes && <p>{pikaMes}</p>}
     </div>
   );
 }
